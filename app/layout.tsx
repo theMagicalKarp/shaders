a41bbe5fcc950b7ecaff754235c848a62f4b0b1e
Rob Sheehy <gameboy1092@gmail.com>
2024-05-15 08:36:08 -0500

    Adjust CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="forest">
+    <html lang="en" data-theme="forest" className="bg-black">
       <body>{children}</body>
     </html>
   );
